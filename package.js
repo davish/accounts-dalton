9907c775ebe052f3ee3bd10f2369e0e2ad2b3422
davis <dbh937@gmail.com>
2015-03-21 22:37:26 -0400

    added accounts-base as a dependency.

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'davish:accounts-dalton',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor login service for the Dalton School',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/dbh937/accounts-dalton.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,6 +13,9 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.4');
   api.addFiles('accounts-dalton.js');
+
+  api.use('accounts-base');
+
 });
 
 Package.onTest(function(api) {
